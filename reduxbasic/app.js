c89d9b3f3a1e52fec698939da71a1a0989bde7b6
enoch232 <enoch232@gmail.com>
2017-01-30 23:23:50 -0700

    add some code

--- reduxbasic/app.js
+++ reduxbasic/app.js
@@ -1,12 +1,23 @@
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
+var redux = require("redux")
+createStore = redux.createStore
 
 const reducer = function( state, action ){
+  if (action.type === "INC"){
+    return state + action.payload
+  }
+  if (action.type === "DEC"){
+    return state - action.payload
+  }
+  return state
 }
 
 const store = createStore(reducer, 0)
 
 store.subscribe(function(){
-  console.log("state changed!")
-}
+  console.log("state changed! the value is now", store.getState())
+}.bind(this))
 
-store.dispatch({type: "INC", value: 0})
+store.dispatch({type: "INC", payload: 5})
+store.dispatch({type: "INC", payload: 7})
+store.dispatch({type: "DEC", payload: 12})
